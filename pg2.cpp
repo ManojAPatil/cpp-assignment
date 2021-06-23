829226ff4073722376f14beeb5388d1dcbe1d8a1
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-06-23 15:38:37 +0530

    assignment2

---
+++ pg2.cpp
@@ -0,0 +1,12 @@
+//prog. to print the sum of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=29,b=30,c;
+  cout<<"Print the sum of two numbers\n";
+  cout<<"-------------------------------\n";
+  c=a+b;
+  cout<<"The sum of "<<a<<" and "<<b<<" is: "<<c<<"\n";
+  return 0;
+}
