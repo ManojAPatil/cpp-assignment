d92876e8ff18af011a039bdffa401cae42b5ab3f
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-06-23 15:41:35 +0530

    assignment7

---
+++ pg7.cpp
@@ -0,0 +1,24 @@
+//prog. to display various type of arithmetic operation using mixed data type
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=5,b=7;
+	double c=3.7,d=8.0;
+	cout<<a<<"+"<<b<<"="<<a+b<<"\n";
+	cout<<c<<"+"<<d<<"="<<c+d<<"\n";
+	cout<<a<<"+"<<d<<"="<<a+d<<"\n";
+	cout<<a<<"-"<<b<<"="<<a-b<<"\n";
+	cout<<c<<"-"<<d<<"="<<c-d<<"\n";
+	cout<<a<<"-"<<d<<"="<<a-d<<"\n";
+	cout<<a<<"*"<<b<<"="<<a*b<<"\n";
+	cout<<c<<"*"<<d<<"="<<c*d<<"\n";
+	cout<<a<<"*"<<d<<"="<<a*d<<"\n";
+	cout<<a<<"/"<<b<<"="<<a/b<<"\n";
+	cout<<c<<"/"<<d<<"="<<c/d<<"\n";
+	cout<<a<<"/"<<d<<"="<<a/d<<"\n";
+	return 0;
+	
+	
+}
