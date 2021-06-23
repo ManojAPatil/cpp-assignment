e6fd594b9272103019c1f1f06b06228bd883ff16
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-06-23 15:39:03 +0530

    assignment3

---
+++ pg3.cpp
@@ -0,0 +1,19 @@
+//Find size of fundamental datatypes
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"find size of fundamental datatypes\n";
+	cout<<"-------------------------------------\n";
+	cout<<"The size of(char) is        =  "<<sizeof(char)<<"byte\n";
+	cout<<"The size of(int) is         =  "<<sizeof(int)<<"byte\n";
+    cout<<"The size of(short) is       =  "<<sizeof(short)<<"byte\n";
+    cout<<"The size of(long) is        =  "<<sizeof(long)<<"byte\n";
+	cout<<"The size of(long long) is   =  "<<sizeof(long long)<<"byte\n";
+	cout<<"The size of(float) is       =  "<<sizeof(float)<<"byte\n";
+	cout<<"The size of(double) is      =  "<<sizeof(double)<<"byte\n";
+	cout<<"The size of(long double) is =  "<<sizeof(long double)<<"byte\n";
+	cout<<"The size of(bool) is        =  "<<sizeof(bool)<<"byte\n";
+		return 0;
+}
