1ea161c4542ca9751ea87151be13e276927661da
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-06-23 15:40:32 +0530

    assignment6

---
+++ pg6.cpp
@@ -0,0 +1,22 @@
+//prog. to check whether the primitive values crossing the limits or not
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char gender='F';
+	bool status=true;
+	int sons=2,Appo_year=2009;
+	long int salary=1500000,sal_drawn=12047235,balance=995324987;
+	double ht=79.48,gpa=4.69;
+	cout<<"The Gender is:"<<gender<<"\n";
+	cout<<"Is she married?:"<<status<<"\n";
+	cout<<"Number of sons she has:"<<sons<<"\n";
+	cout<<"Year of her appointment:"<<Appo_year<<"\n";
+	cout<<"Salary of year:"<<salary<<"\n";
+	cout<<"Height:"<<ht<<"\n";
+	cout<<"GPA is:"<<gpa<<"\n";
+	cout<<"Salary drawn upto:"<<sal_drawn<<"\n";
+	cout<<"Balance till:"<<balance<<"\n";
+	return 0;
+}
