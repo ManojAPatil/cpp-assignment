560c191d1f14a30a36fb2d84f5720792b632781c
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-06-25 15:17:00 +0530

    assignment33

---
+++ pg33.cpp
@@ -0,0 +1,16 @@
+/*Write a program in C++ to divide two numbers and print on the screen.
+Sample Output:
+Divide two numbers and print:
+----------------------------------
+The quotient of 30 and 10 is : 3*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+     int a=30,b=10,c;
+     c=a/b;
+     cout<<"The quotient of"<<a<<"and"<<b<<"is :"<<c<<endl;
+return 0;
+}
+
