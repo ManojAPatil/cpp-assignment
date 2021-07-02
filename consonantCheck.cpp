927d610cfb378266faf47171e077c6aa9f6decd4
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-02 15:08:41 +0530

    Add files via upload

---
+++ consonantCheck.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      char c;
+      cout<<"enter the character : ";
+      cin>>c;
+      if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+      {
+      	cout<<c<<" is Consonant\n";
+	  }else{
+	        cout<<c<<" is not Consonant\n";
+	   }
+	  
+return 0;
+}
