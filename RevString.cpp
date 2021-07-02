3306c8545b9e020e6a0c0de19acb1bde828aab20
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-02 15:06:43 +0530

    Add files via upload

---
+++ RevString.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+     string s,ss;
+     int a,temp;
+    
+     cout<<"Enter the string : ";
+     cin>>s;
+     
+     a=s.length()-1;
+     for(int i=0;i<a;i++)
+     {
+     	temp=s[i];
+     	s[i]=s[a];
+     	s[a]=temp;
+     	a--;
+	 }
+	 cout<<"Reverse string is : "<<s<<endl;
+return 0;
+}
